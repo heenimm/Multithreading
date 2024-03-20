05dfa25bae5ae9d87f2679a32fd3233a045295b4
Halimat <m.h.axm@mail.ru>
2024-03-20 13:29:35 +0300

    fix bug

---
+++ Multithreading/Multithreading/Multithreading/IsCancelledViewController.swift
@@ -0,0 +1,31 @@
+//
+//  IsCancelledViewController.swift
+//  Multithreading
+//
+//  Created by Халимка on 20.03.2024.
+//
+
+import UIKit
+
+class IsCancelledViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let infinityThread = InfinityLoop()
+        infinityThread.start()
+        sleep(2)
+        infinityThread.isCancelled
+    }
+}
+
+class InfinityLoop: Thread {
+    var counter = 0
+    
+    override func main() {
+        while counter < 30 && !isCancelled {
+            counter += 1
+            print(counter)
+            InfinityLoop.sleep(forTimeInterval: 1)
+        }
+    }
+}
